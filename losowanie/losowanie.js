d6604a85793aef59816b711743b092f40a19faa1
Wojciech Karpiel <w.karpiel@o2.pl>
2022-10-02 18:42:20 +0200

    jestę frątędę

--- losowanie/losowanie.js
+++ losowanie/losowanie.js
@@ -1,21 +1,20 @@
 var ANIA = "Ania"
-var JAMA = "Jama"
 var SAGA = "Saga"
 var TOMEK = "Tomek"
 var WOJTEK = "Wojtek"
 var PAULINKA = "Paulinka"
-var LUDZIE = [ ANIA, JAMA, SAGA, TOMEK, WOJTEK, PAULINKA ]
-
-var WYLOSOWANE = { Ania: 89, Jama: 165, Saga: 16, Tomek: 39, Wojtek: 202, Paulinka: 179 }
-// do usunięcia xD
-HASLA = {}
-HASLA[ANIA]= 92
-HASLA[JAMA]= 167
-HASLA[SAGA]= 20
-HASLA[TOMEK]= 38
-HASLA[WOJTEK]= 201
-HASLA[PAULINKA]= 179
-
+var KAPUSTA = "Kapusta"
+var WACEK = "Wacek"
+var LUDZIE = [ ANIA, SAGA, TOMEK, WOJTEK, PAULINKA , KAPUSTA, WACEK]
+var MX = (function(){
+    var i=1;
+    while (i < LUDZIE.length){
+        i=i*2;
+    }
+    return i;
+})()
+var WYLOSOWANE = 
+ "eyIwIjowLCIxIjo1LCIyIjoyLCIzIjowLCI0Ijo1LCI1Ijo1LCI2IjozfQ=="
 
 var ZAKAZANE_PARY = {}
 function dodaj_zakaz(osoba1, osoba2) {
@@ -23,7 +22,7 @@ function dodaj_zakaz(osoba1, osoba2) {
     ZAKAZANE_PARY[osoba2] = osoba1
 }
 
-dodaj_zakaz(ANIA, JAMA)
+//dodaj_zakaz(ANIA, JAMA)
 dodaj_zakaz(SAGA, TOMEK)
 
 
@@ -50,65 +49,31 @@ function znajdz_nr(osoba){
     }
 }
 
-function ph(h) {
-    return h;
-}
 
 function wylosuj_spoko(){
     var i = 0
+    var HASLA = {}
+    for(i=0;i<LUDZIE.length;i++){
+        HASLA[LUDZIE[i]] = Math.floor(Math.random() * MX); 
+    }
+    i =0;
     while(true){
         p = losowa_permutacja(LUDZIE)
         if (dobra_permutacja(p)){
             console.log("Zajęło " + i)
-            console.log(LUDZIE)
-            console.log(p)
             odp = {}
             var j=0
             for (j=0; j < LUDZIE.length; j++){
                 var l = LUDZIE[j]
-                console.log("ogarniam " + l);
-                odp[l] =  zaszyfruj( znajdz_nr(p[j]), HASLA[l])
+                odp[j] =  zaszyfruj( znajdz_nr(p[j]), HASLA[l])
             }
-            return odp
+
+            // odp b64 tylko po to, żebym nie widział rozwiązania od razu, to nie szyfr
+            return {"odp": btoa(JSON.stringify( odp)), "hasła": HASLA}
         }
         i++
     }
 }
-
-/*
-  function zaszyfruj(numerek, haslo) {
-
-  var key = new TextEncoder().encode(ph(haslo));
-
-  //    var text = wartosc
-  var textBytes = [ numerek ]; //  aesjs.utils.utf8.toBytes(text);
-
-  // The counter is optional, and if omitted will begin at 1
-  var aesCtr = new aesjs.ModeOfOperation.ctr(key, new aesjs.Counter(5));
-  var encryptedBytes = aesCtr.encrypt(textBytes);
-
-  // To print or store the binary data, you may convert it to hex
-  var encryptedHex = aesjs.utils.hex.fromBytes(encryptedBytes);
-  return encryptedHex
-  }
-
-  function odszyfruj(zaszyfrowane, haslo){
-  var key = new TextEncoder().encode(ph(haslo));
-  // When ready to decrypt the hex string, convert it back to bytes
-  var encryptedBytes = aesjs.utils.hex.toBytes(zaszyfrowane);
-
-  // The counter mode of operation maintains internal state, so to
-  // decrypt a new instance must be instantiated.
-  var aesCtr = new aesjs.ModeOfOperation.ctr(key, new aesjs.Counter(5));
-  var decryptedBytes = aesCtr.decrypt(encryptedBytes);
-
-  // Convert our bytes back into text
-  var decryptedText = decryptedBytes[0] // aesjs.utils.utf8.fromBytes(decryptedBytes);
-  return decryptedText
-  // "Text may be any length you wish, no padding is required."
-  }
-*/
-
 
 
 
@@ -131,9 +96,19 @@ function przygotuj(){
     }
 
 
-    var input = document.getElementById('haslo');
+
+var input = document.getElementById('haslo');
+    for (var i = 0; i< LUDZIE.length; i++){
+        var opt = document.createElement('option');
+        opt.value = i;
+        opt.innerHTML = i;
+        input.appendChild(opt);
+    }
+
+
+    
     input.addEventListener('input', function (e) {kogo_ogar() ;});
-    ja.addEventListener('change', function (e) {kogo_ogar() ;});
+    ja.addEventListener('input', function (e) {kogo_ogar() ;});
     kogo_ogar();
 }
 
@@ -142,15 +117,22 @@ function kogo_ogar(){
     wynik.innerHTML = "ELO"
     
     var select=    document.getElementById('ja');
-    var ja =+ select.options[select.selectedIndex].value;
+    var jaq = select.options[select.selectedIndex].value;
 
-    var haslo = + document.getElementById('haslo').value.trim();
+    var halo = document.getElementById('haslo').value.trim()
+    
 
-    if (haslo == ''){
+
+    // KTO WYMYŚLIŁ, ŻE 0 == '' XDDDDD
+    if (halo === '' || halo === ' ' || jaq === '' || jaq === ' ' ){
         wynik.innerHTML=""
     }else{
         try {
-            var o = odszyfruj(WYLOSOWANE[LUDZIE[ja]], haslo)
+                var    ja = + jaq;
+                var haslo = +(halo);
+            var xx = JSON.parse( atob(WYLOSOWANE))
+          
+            var o = odszyfruj(xx[ja], haslo)
             o=LUDZIE[o];
             if (o != undefined && o != null && o != "undefined" ){
                 wynik.innerHTML=o
