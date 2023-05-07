581b056f1845b897edcff4708a09d3a4f3f3873e
Wojciech Karpiel <w.karpiel@o2.pl>
2023-05-07 12:34:25 +0200

    obsługa youtu.be/{id}

--- yt/yt.js
+++ yt/yt.js
@@ -1,7 +1,13 @@
 document.addEventListener("DOMContentLoaded", function(){
     const source = document.getElementById('sznurek');
     const result = document.getElementById('wynik');
-
+    const countInstances = function(string, word) {
+        return string.split(word).length - 1;
+    }
+    const getHtml = function(v){
+        const url = "https://www.youtube-nocookie.com/embed/" + v;
+        return '<div><a href="'+url+'">'+url+'</a></div><div><iframe width="560" height="315" src="'+url+'" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>'
+    }
     const inputHandler = function(e) {
         var resultHtml = "";
         var f = null;
@@ -11,7 +17,7 @@ document.addEventListener("DOMContentLoaded", function(){
             f = document.getElementById('sznurek');
         }
         const inputValue = f.value;
-
+        
         if (inputValue.length === 0) {
             resultHtml = "";
         }else {
@@ -27,10 +33,15 @@ document.addEventListener("DOMContentLoaded", function(){
             } else{
                 const vQuery = url.searchParams.get('v')
                 if (vQuery != null && vQuery.length > 0) {
-                    const url = "https://www.youtube-nocookie.com/embed/" + vQuery
-                    resultHtml='<div><a href="'+url+'">'+url+'</a></div><div><iframe width="560" height="315" src="'+url+'" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>'
+                    resultHtml= getHtml(vQuery)
                 }else{
-                    resultHtml="To na pewno sznurek do filmu na YT?"
+                    const p = url.pathname;
+                    if (p != null && p.startsWith("/") && p.length>1 && countInstances(p,"/")==1 ){
+                        const vq = p.substring(1)
+                        resultHtml=getHtml(vq);
+                    }else{
+                        resultHtml="To na pewno sznurek do filmu na YT?"
+                    }
                 }
             }
             
